4d20ba344df417a1d61654cca9dfb064411ed83d
toporowitch <4158847@gmail.com>
2021-05-16 02:10:26 -0400

    Day 1 - band name generator

---
+++ day1/band_name_generator.py
@@ -0,0 +1,4 @@
+print('Welcome to the band name generator')
+city = input('What the name of the city you grew up in?\n')
+pet_name = input('What the name of your pat\'s name?\n')
+print(f'Your band name could be {city.capitalize()} {pet_name.capitalize()}')
